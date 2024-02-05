da2a3ad742387eed6cd68a00f778148ef7e8bdd6
SoLegendary <nick.tangson@gmail.com>
2024-02-05 23:41:32 +1100

    mixin refactoers

--- src/main/java/com/solegendary/reignofnether/mixin/fogofwar/ModelBlockRendererMixin.java
+++ src/main/java/com/solegendary/reignofnether/mixin/fogofwar/ModelBlockRendererMixin.java
@@ -3,6 +3,7 @@ package com.solegendary.reignofnether.mixin.fogofwar;
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.solegendary.reignofnether.fogofwar.FogOfWarClientEvents;
+import net.minecraft.client.color.block.BlockColors;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.block.ModelBlockRenderer;
 import net.minecraft.client.renderer.block.model.BakedQuad;
@@ -10,6 +11,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.world.level.BlockAndTintGetter;
 import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,35 +22,48 @@ import javax.annotation.Nullable;
 import java.util.BitSet;
 import java.util.List;
 
-// brightness shading for blocks excluding liquids and flat flace blocks (like tall grass)
+// brightness shading for liquids and flat flace blocks (like tall grass)
 
 @Mixin(ModelBlockRenderer.class)
 public abstract class ModelBlockRendererMixin {
 
-    @Shadow private void calculateShape(BlockAndTintGetter pLevel, BlockState pState, BlockPos pPos, int[] pVertices, Direction pDirection, @Nullable float[] pShape, BitSet pShapeFlags) {}
-    @Shadow private void putQuadData(BlockAndTintGetter pLevel, BlockState pState, BlockPos pPos, VertexConsumer pConsumer, PoseStack.Pose pPose, BakedQuad pQuad, float pBrightness0, float pBrightness1, float pBrightness2, float pBrightness3, int pLightmap0, int pLightmap1, int pLightmap2, int pLightmap3, int pPackedOverlay) {}
+    @Shadow @Final private BlockColors blockColors;
 
     @Inject(
-        method = "renderModelFaceFlat",
-        at = @At("HEAD"),
-        cancellable = true
+            method = "putQuadData",
+            at = @At("HEAD"),
+            cancellable = true
     )
-    private void renderModelFaceFlat(BlockAndTintGetter pLevel, BlockState pState, BlockPos pPos, int pPackedLight, int pPackedOverlay, boolean pRepackLight,
-                                     PoseStack pPoseStack, VertexConsumer pConsumer, List<BakedQuad> pQuads, BitSet pShapeFlags, CallbackInfo ci) {
-        if (!FogOfWarClientEvents.isEnabled())
+    private void putQuadData(BlockAndTintGetter pLevel, BlockState pState, BlockPos pPos, VertexConsumer pConsumer, PoseStack.Pose pPose,
+                                     BakedQuad pQuad, float pBrightness0, float pBrightness1, float pBrightness2, float pBrightness3,
+                                     int pLightmap0, int pLightmap1, int pLightmap2, int pLightmap3, int pPackedOverlay, CallbackInfo ci) {
+        boolean allBrightnesses1 =
+                pBrightness0 == pBrightness1 &&
+                pBrightness1 == pBrightness2 &&
+                pBrightness2 == pBrightness3 &&
+                pBrightness3 == 1.0f;
+
+        if (!FogOfWarClientEvents.isEnabled() || !allBrightnesses1)
             return;
 
         ci.cancel();
 
-        for(BakedQuad bakedquad : pQuads) {
-            if (pRepackLight) {
-                this.calculateShape(pLevel, pState, pPos, bakedquad.getVertices(), bakedquad.getDirection(), null, pShapeFlags);
-                BlockPos blockpos = pShapeFlags.get(0) ? pPos.relative(bakedquad.getDirection()) : pPos;
-                pPackedLight = LevelRenderer.getLightColor(pLevel, pState, blockpos);
-            }
-
-            float f = pLevel.getShade(bakedquad.getDirection(), bakedquad.isShade()) * FogOfWarClientEvents.getPosBrightness(pPos);
-            this.putQuadData(pLevel, pState, pPos, pConsumer, pPoseStack.last(), bakedquad, f, f, f, f, pPackedLight, pPackedLight, pPackedLight, pPackedLight, pPackedOverlay);
+        float f;
+        float f1;
+        float f2;
+        if (pQuad.isTinted()) {
+            int i = this.blockColors.getColor(pState, pLevel, pPos, pQuad.getTintIndex());
+            f = (float)(i >> 16 & 255) / 255.0F;
+            f1 = (float)(i >> 8 & 255) / 255.0F;
+            f2 = (float)(i & 255) / 255.0F;
+        } else {
+            f = 1.0F;
+            f1 = 1.0F;
+            f2 = 1.0F;
         }
+        float br = FogOfWarClientEvents.getPosBrightness(pPos);
+
+        pConsumer.putBulkData(pPose, pQuad, new float[]{pBrightness0 * br, pBrightness1 * br, pBrightness2 * br, pBrightness3 * br},
+                f, f1, f2, new int[]{pLightmap0, pLightmap1, pLightmap2, pLightmap3}, pPackedOverlay, true);
     }
 }
