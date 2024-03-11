82da0e9c7ba9ba60f77bfbfdcb2b5a7568d0946a
SoLegendary <nick.tangson@gmail.com>
2024-03-11 23:05:40 +1100

    ravager buff

--- src/main/java/com/solegendary/reignofnether/unit/units/villagers/RavagerUnit.java
+++ src/main/java/com/solegendary/reignofnether/unit/units/villagers/RavagerUnit.java
@@ -127,7 +127,7 @@ public class RavagerUnit extends Ravager implements Unit, AttackerUnit {
 
     final static public float attackDamage = 7.0f;
     final static public float attacksPerSecond = 0.6f;
-    final static public float maxHealth = 200.0f;
+    final static public float maxHealth = 300.0f;
     final static public float armorValue = 0.0f;
     final static public float movementSpeed = 0.28f;
     final static public float attackRange = 2; // only used by ranged units or melee building attackers
