392a1a7000c30ffaf89c960c3babf675df6a4602
SoLegendary <nick.tangson@gmail.com>
2024-05-07 20:26:45 +1000

    frozen chunks rework

--- src/main/java/com/solegendary/reignofnether/fogofwar/FrozenChunk.java
+++ src/main/java/com/solegendary/reignofnether/fogofwar/FrozenChunk.java
@@ -49,38 +49,42 @@ public class FrozenChunk {
     public void saveBlocks() {
         if (MC.level == null)
             return;
+
+
+
         for (int x = 0; x <= 16; x++) {
             for (int y = 0; y <= 16; y++) {
                 for (int z = 0; z <= 16; z++) {
                     BlockPos bp = origin.offset(x,y,z);
-                    if (MC.level.isLoaded(bp)) {
-                        // if we tried to save this previously but it was unloaded, so manually replace certain blocks:
-                        // 1. Scaffolding -> Air
-                        // 2. Magma/Cobble -> Coal Ore
-                        // 3. Nether Blocks -> Overworld equivalent
-                        // 4. Blocks that are a part of the parent building -> Air
-                        if (attemptedUnloadedSave) {
-                            saveFakeBuildingBlocks = true;
-                            BlockState bs = MC.level.getBlockState(bp);
-                            String blockName = bs.getBlock().getName().getString().toLowerCase();
-                            if (NetherBlocks.isNetherBlock(MC.level, bp)) {
-                                BlockState overworldBs = NetherBlocks.getOverworldBlock(MC.level, bp);
-                                if (overworldBs != null)
-                                    blocks.add(new Pair<>(bp, overworldBs));
-                            } else if (blockName.equals("scaffolding")) {
-                                blocks.add(new Pair<>(bp, Blocks.AIR.defaultBlockState()));
-                            } else if (blockName.equals("magma_block") ||
-                                       blockName.equals("cobblestone") ) {
-                                blocks.add(new Pair<>(bp, Blocks.COAL_ORE.defaultBlockState()));
-                            } else if (blockName.equals("dirt")) {
-                                blocks.add(new Pair<>(bp, Blocks.GRASS_BLOCK.defaultBlockState()));
-                            }
-                            hasFakeBlocks = true;
-                        } else {
-                            BlockState bs = MC.level.getBlockState(bp);
-                            blocks.add(new Pair<>(bp, MC.level.getBlockState(bp)));
-                            hasFakeBlocks = false;
+
+                    // if we tried to save this previously but it was unloaded, so manually replace certain blocks:
+                    // 1. Scaffolding -> Air
+                    // 2. Magma/Cobble -> Coal Ore
+                    // 3. Nether Blocks -> Overworld equivalent
+                    // 4. Blocks that are a part of the parent building -> Air
+                    if (attemptedUnloadedSave) {
+                        saveFakeBuildingBlocks = true;
+                        BlockState bs = MC.level.getBlockState(bp);
+                        String blockName = bs.getBlock().getName().getString().toLowerCase();
+                        if (blockName.equals("scaffolding")) {
+                            blocks.add(new Pair<>(bp, Blocks.AIR.defaultBlockState()));
+                        } else if (blockName.equals("magma_block") ||
+                                blockName.equals("cobblestone") ) {
+                            blocks.add(new Pair<>(bp, Blocks.COAL_ORE.defaultBlockState()));
+                        } else if (blockName.equals("dirt")) {
+                            blocks.add(new Pair<>(bp, Blocks.GRASS_BLOCK.defaultBlockState()));
+                        } else if (blockName.equals("obsidian")) {
+                            blocks.add(new Pair<>(bp, Blocks.WATER.defaultBlockState()));
+                        } else if (NetherBlocks.isNetherBlock(MC.level, bp)) {
+                            BlockState overworldBs = NetherBlocks.getOverworldBlock(MC.level, bp);
+                            if (overworldBs != null)
+                                blocks.add(new Pair<>(bp, overworldBs));
                         }
+                        hasFakeBlocks = true;
+                    } else {
+                        BlockState bs = MC.level.getBlockState(bp);
+                        blocks.add(new Pair<>(bp, MC.level.getBlockState(bp)));
+                        hasFakeBlocks = false;
                     }
                 }
             }
@@ -106,5 +110,16 @@ public class FrozenChunk {
         for (Pair<BlockPos, BlockState> pair : blocks) {
             MC.level.setBlockAndUpdate(pair.getFirst(), pair.getSecond());
         }
+    }
+
+    private boolean isFullyLoaded() {
+        if (MC.level == null)
+            return false;
+        for (int x = 0; x <= 16; x++)
+            for (int y = 0; y <= 16; y++)
+                for (int z = 0; z <= 16; z++)
+                    if (!MC.level.isLoaded(origin.offset(x,y,z)))
+                        return false;
+        return true;
     }
 }
