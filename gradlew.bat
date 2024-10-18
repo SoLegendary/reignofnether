562da44b517f818527152bced62c5157f97ccbec
maaaxinfinity <3206288040@qq.com>
2024-10-18 22:18:07 +0800

    modified:   gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -14,79 +14,76 @@
 @rem limitations under the License.
 @rem
 
-@echo off
-chcp 65001 >nul
-
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
+@rem
 @rem  Gradle startup script for Windows
+@rem
 @rem ##########################################################################
 
-@rem Set local scope for the variables with Windows NT shell
+@rem Set local scope for the variables with windows NT shell
 if "%OS%"=="Windows_NT" setlocal
 
-set "DIRNAME=%~dp0"
-if "%DIRNAME%" == "" set "DIRNAME=."
-set "APP_BASE_NAME=%~n0"
-set "APP_HOME=%DIRNAME%"
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
-for %%i in ("%APP_HOME%") do set "APP_HOME=%%~fi"
+for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
 @rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS=-Xmx64m -Xms64m
+set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
 
-@rem 查找 java.exe
-
+@rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
 
-set "JAVA_EXE=java.exe"
-"%JAVA_EXE%" -version >NUL 2>&1
-if "%ERRORLEVEL%"=="0" goto execute
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto execute
 
 echo.
-echo 错误: JAVA_HOME 未设置，并且在 PATH 中找不到 'java' 命令。
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
 echo.
-echo 请在环境变量中设置 JAVA_HOME，以匹配你安装的 Java 位置。
-echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
 goto fail
 
 :findJavaFromJavaHome
-rem 去掉 JAVA_HOME 中的引号
-set "JAVA_HOME=%JAVA_HOME:"=%"
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-set "JAVA_EXE=%JAVA_HOME%\bin\java.exe"
+if exist "%JAVA_EXE%" goto execute
 
-if exist "%JAVA_EXE%" (
-    goto execute
-) else (
-    echo.
-    echo 错误: JAVA_HOME 设置为无效目录：%JAVA_HOME%
-    echo.
-    goto fail
-)
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
+
+goto fail
 
 :execute
-rem 设置类路径
-set "CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar"
+@rem Setup the command line
 
-rem 合并所有 JVM 选项
-set "ALL_JVM_OPTS=%DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS%"
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
 
-rem 执行 Gradle
-echo 运行 Gradle...
-"%JAVA_EXE%" %ALL_JVM_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
-goto end
-
-:fail
-rem 如果需要脚本的返回码而不是 cmd.exe 的返回码，请设置 GRADLE_EXIT_CONSOLE
-if not ""=="%GRADLE_EXIT_CONSOLE%" exit 1
-exit /b 1
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
 
 :end
-rem 清理
+@rem End local scope for the variables with windows NT shell
+if "%ERRORLEVEL%"=="0" goto mainEnd
+
+:fail
+rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+rem the _cmd.exe /c_ return code!
+if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
+exit /b 1
+
+:mainEnd
 if "%OS%"=="Windows_NT" endlocal
 
 :omega
-rem 脚本结束
