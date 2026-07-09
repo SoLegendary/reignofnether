10301aaae9b5f756b02647cc14c230c9e8d0312e
SoLegendary <nick.tangson@gmail.com>
2026-07-09 20:29:12 +1000

    stair climb bug fix

--- src/main/java/com/solegendary/reignofnether/unit/units/monsters/SpiderUnit.java
+++ src/main/java/com/solegendary/reignofnether/unit/units/monsters/SpiderUnit.java
@@ -199,6 +199,7 @@ public class SpiderUnit extends Spider implements Unit, AttackerUnit, Convertabl
 
     public SpiderUnit(EntityType<? extends Spider> entityType, Level level) {
         super(entityType, level);
+        this.setMaxUpStep(1.15F);
     }
 
     public void toggleWallClimbing() {
